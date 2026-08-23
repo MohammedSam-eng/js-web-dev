2b0a90fe5f57956f20058f7a00ca94eb2d65d531
MohammedSam-eng <sameerim18a@gmail.com>
2026-08-23 18:19:37 +0000

    practice problems

---
+++ 01_javascript/dailypractice.js
@@ -0,0 +1,12 @@
+//Question 1
+let myname = "Mohammed"
+let age = 20
+let college = "ISL engineering college"
+let branch = "Cse"
+let cgpa = 7.5
+console.log(myname)
+console.log(age)
+console.log(college)
+console.log(branch)
+console.log(cgpa)
+console.table([myname,age,college,branch,cgpa])

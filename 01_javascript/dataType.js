73f55b50ba0a47f7e9ff692740f91cb55f02873a
MohammedSam-eng <sameerim18a@gmail.com>
2026-08-23 17:54:49 +0000

    DataTypes

---
+++ 01_javascript/dataType.js
@@ -0,0 +1,16 @@
+"use strict"; // Treat all JS code as newer version
+//alert(3 +4)// we are using node js not browser
+//DataTypes
+/*
+number
+bigint
+string
+boolean
+null=> standalone value
+undefined=> value not yet assigned
+symbol
+*/
+let name = " sameer "
+console.log(typeof "sameer") //it will return the type of data which is mentioned in quotes
+console.log(typeof null) // it is a object type
+console.log(typeof undefined) // it gives undifined only as it is a special datatype as string or others

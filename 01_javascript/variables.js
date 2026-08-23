2f50bf9aaf721d3584d2f5e8d1d00aa138d94c75
MohammedSam-eng <sameerim18a@gmail.com>
2026-08-23 11:33:31 +0000

    variables

---
+++ 01_javascript/variables.js
@@ -0,0 +1,11 @@
+const accountId = 12323
+let accountEmail = "sameer@google.com"
+var accountPassword = "12112"
+accountCity = "hyderabad"
+let accountState;
+accountEmail="sam@google.com"
+console.table([accountEmail,accountId,accountPassword,accountState,accountCity])
+/*
+prefer not to use var 
+because of issues in block scope and functional scope
+*/

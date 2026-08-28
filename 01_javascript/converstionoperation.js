2e24791aff77cf7be3c4b45444db93573d96aac2
MohammedSam-eng <sameerim18a@gmail.com>
2026-08-28 11:33:09 +0000

    converstions

--- 01_javascript/converstionoperation.js
+++ 01_javascript/converstionoperation.js
@@ -1,1 +1,38 @@
-let marks 
+let score = "33asdf"
+/*
+console.log(typeof score)
+let valueINNumber = Number(score)
+console.log(typeof valueINNumber)
+console.log(valueINNumber)
+/*
+we can convert any string into number or integer with Number()
+and we can see its type as number when we insert the variable into typeof function
+but if we try to print the value of the variable which has both number and alphabets it will show NaN
+NaN=Not an Number
+so we cannot print the value of a variable which has both number and alphabet
+*/
+/*
+let score1 = null
+let score2 = undefined
+let score3 = true
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+console.table([valueInNumber1,valueInNumber2,valueInNumber3])
+console.log(typeof(valueInNumber1))
+console.log(typeof(valueInNumber2))
+console.log(typeof(valueInNumber3))
+*/
+/*
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1 => true; 0 => false;
+// "" => false;
+// "sam" => true;
+*/
+let someNumber = 46
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
